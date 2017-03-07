f4f2ff86d80f48176d10e42213e295bd27ab6dd5
balthair94 <archerbaltazar@gmail.com>
2017-03-06 22:52:08 -0600

    Firebase added to the page

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+//FIREBASE
+var config = {
+    apiKey: "AIzaSyCc881NFW8fdbftLA5xD1OesxBXNHrAJSY",
+    authDomain: "mhlocationdatabase-e7c61.firebaseapp.com",
+    databaseURL: "https://mhlocationdatabase-e7c61.firebaseio.com",
+    storageBucket: "mhlocationdatabase-e7c61.appspot.com",
+    messagingSenderId: "650519975440"
+  	};
+firebase.initializeApp(config);
